803a00d73932b64345b13ec67cea77a9afeb3d2d
TomohiroHiratsuka <tomtombaske@gmail.com>
2019-04-12 14:46:37 +0900

    mit3_2 fixed and mit3_3 done

--- mit3_3.py
+++ mit3_3.py
@@ -1,23 +1,28 @@
 import sys
 
+print(sys.argv)
+x = int(sys.argv[1])
+y = int(sys.argv[2])
+z = int(sys.argv[3])
+
 bestSoFar = 0  # variable that keeps track of largest number
 # of McNuggets that cannot be bought in exact quantity
-packages = (6, 9, 20)  # variable that contains package sizes
+packages = (x, y, z)  # variable that contains package sizes
 ans = {}
-candidates = list(range(1, 151))
-for n in range(1, 151):  # only search for solutions up to size 150
+candidates = list(range(1, 201))
+for n in range(1, 201):  # only search for solutions up to size 150
     ## complete code here to find largest size that cannot be bought
     ## when done, your answer should be bound to bestSoFa
-    a = list(range(int((n / 6) + 1)))
-    b = list(range(int((n / 9) + 1)))
-    c = list(range(int((n / 20) + 1)))
+    a = list(range(int((n / x) + 1)))
+    b = list(range(int((n / y) + 1)))
+    c = list(range(int((n / z) + 1)))
 
     for candidate_a in a:
         # print(candidate_a)
         for candidate_b in b:
             # print(candidate_b)
-            c = (n - ((6 * candidate_a) + (9 * candidate_b))) / 20
-            check_c = (n - ((6 * candidate_a) + (9 * candidate_b))) % 20
+            c = (n - ((x * candidate_a) + (y * candidate_b))) / z
+            check_c = (n - ((x * candidate_a) + (y * candidate_b))) % z
 
             if check_c == 0 and c >= 0:
                 c = int(c)
@@ -32,5 +37,6 @@ for n in range(1, 151):  # only search for solutions up to size 150
             continue
         break
 
-text = "Largest number of McNuggets that cannot be bought in exact quantity:" + str(bestSoFar)
+text = "Given package sizes {0}, {1}, and {2}, the largest number of McNuggets that cannot be bought in exact quantity is:{3}".format(
+    x, y, z, bestSoFar)
 print(text)
